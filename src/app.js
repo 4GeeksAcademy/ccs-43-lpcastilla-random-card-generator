6bcc02abd93505639cc181ff636d52d552ad5ea0
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-06-14 00:51:57 +0000

    se modificaron los archivos app, index y style

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const element1 = document.querySelectorAll(".type");
+  const element2 = document.querySelector(".number");
+  const element3 = document.querySelector(".btn");
+
+  let type = [
+    "<p class='text-red'>♦</p>",
+    "<p class='text-red'>♥</p>",
+    "♠",
+    "♣"
+  ];
+
+  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  function myClickHandler() {
+    let randomIndexType = Math.floor(Math.random() * type.length);
+    let randomType = type[randomIndexType];
+
+    for (let element of element1) {
+      element.classList.add("font-xl");
+      element.innerHTML = randomType;
+    }
+
+    let randomIndexNumber = Math.floor(Math.random() * number.length);
+    let randomNumber = number[randomIndexNumber];
+
+    element2.innerHTML = randomNumber;
+    element3.addEventListener("click", myClickHandler);
+  }
+
+  myClickHandler();
 };
